bf2866bc17feb63fa0a35396c3a5f4798a83eb84
lompdoctor <pleclair@protonmail.com>
2022-06-29 08:37:02 -0500

    Adjust info--container, buttons, and description styling

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 function About() {
   return (
     <div className='about--container'>
-        <h2>About</h2>
+        <h2 className='description--header'>About</h2>
         <p>Here's a paragraph about me. You'd probably like to know that I'm particularly interested in JavaScript (ES6 / ReactJS), HTML, & CSS!</p>
     </div>
   );
--- src/Components/Interests.js
+++ src/Components/Interests.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Interests() {
   return (
     <div className='interests--container'>
-        <h2>Interests</h2>
+        <h2 className='description--header'>Interests</h2>
         <p>Space travel, blockchain, life extension, UI/UX, strong coffee.</p>
     </div>
   );
